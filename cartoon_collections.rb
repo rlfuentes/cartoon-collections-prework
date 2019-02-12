5ad83816bfdf02063edf6eb4c5b3ff2d27a4afa7
Robert <r_lee94@yahoo.com>
2019-02-12 20:29:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,13 +3,17 @@ def roll_call_dwarves(dwarves)
     index += 1
     puts "#{index}. #{name}"
 end
-
+end 
 def def summon_captain_planet(planeteer_calls)
   planeteer_calls.collect do |call|
     call.capitalize << "!"
 end
+end
 
-def 
+def ong_planeteer_calls(calls)
+  calls.any? do |call|
+    call.length > 4
+end
 end
 
 def find_the_cheese# code an argument here
