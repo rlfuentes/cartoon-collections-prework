56253dd5ada26edbd08819e87b87899a05b8e862
Robert <r_lee94@yahoo.com>
2019-02-12 20:28:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,7 +4,9 @@ def roll_call_dwarves(dwarves)
     puts "#{index}. #{name}"
 end
 
-def 
+def def summon_captain_planet(planeteer_calls)
+  planeteer_calls.collect do |call|
+    call.capitalize << "!"
 end
 
 def long_planeteer_calls# code an argument here
