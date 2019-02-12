77051448a791bf12d3a84fb12274769cd2c65490
Robert <r_lee94@yahoo.com>
2019-02-12 20:30:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,5 @@ def ong_planeteer_calls(calls)
 end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese
 end
